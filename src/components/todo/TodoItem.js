0a33d112fc4118a4b542fce5701b2e35be4271ba
Christiaan Westerbeek <chris@devotis.nl>
2017-05-08 19:13:39 +0200

    Done with 7th video

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const TodoItem = (props) => {
   return (
     <li>
       <input
         type="checkbox"
-        defaultChecked={props.todo.isComplete}
-      /> {props.todo.name}</li>
+        defaultChecked={props.isComplete}
+      /> {props.name}</li>
   )
+}
+
+TodoItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  isComplete: PropTypes.bool
 }
