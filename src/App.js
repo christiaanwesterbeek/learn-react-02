410a7911e1f969b3933ee9952c1b83fe3e33d4d3
Christiaan Westerbeek <chris@devotis.nl>
2017-05-10 22:15:19 +0200

    Done with 24th and last video

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo'
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers'
 import {partial, pipe} from './lib/utils'
-import {loadTodos, createTodo, saveTodo} from './lib/todoService'
+import {loadTodos, createTodo, saveTodo, destroyTodo} from './lib/todoService'
 
 class App extends Component {
   state = {
@@ -29,6 +29,9 @@ class App extends Component {
     this.setState({
       todos: updatedTodos
     })
+
+    destroyTodo(id)
+      .then(() => this.showTempMessage('Todo removed'))
   }
 
   handleToggle = (id) => {
