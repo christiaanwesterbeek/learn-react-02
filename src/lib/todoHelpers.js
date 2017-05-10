3887e3c5babc4155b37f4c1703324ab6a47369b1
Christiaan Westerbeek <chris@devotis.nl>
2017-05-10 10:15:00 +0200

    Done with 12th video

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,11 @@
 export const addTodo = (list, item) => [...list, item]
 
 export const generateId = () => Math.floor(Math.random()*100000)
+
+export const findById = (id, list) => list.find(item => item.id === id)
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete})
+
+export const updateTodo = (list, updated) => {
+  return list.map((todo) => todo.id === updated.id ? {...updated} : {...todo})
+}
